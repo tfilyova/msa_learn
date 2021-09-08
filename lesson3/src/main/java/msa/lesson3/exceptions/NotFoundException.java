a2e7db1c3dd6e47cc04cb56c30b39516f52247f5
tfilyova <88984709+tfilyova@users.noreply.github.com>
2021-09-08 09:12:26 +0300

    Delete lesson3 directory

--- lesson3/src/main/java/msa/lesson3/exceptions/NotFoundException.java
+++
@@ -1,8 +0,0 @@
-package msa.lesson3.exceptions;
-
-import org.springframework.http.HttpStatus;
-import org.springframework.web.bind.annotation.ResponseStatus;
-
-@ResponseStatus(value = HttpStatus.NOT_FOUND)
-public class NotFoundException extends RuntimeException {
-}
