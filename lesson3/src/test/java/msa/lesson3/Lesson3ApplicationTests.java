cc690f123c7c08397263cfbc937d8d9713379eb6
tfilyova <88984709+tfilyova@users.noreply.github.com>
2021-09-09 09:03:04 +0300

    lesson3

---
+++ lesson3/src/test/java/msa/lesson3/Lesson3ApplicationTests.java
@@ -0,0 +1,13 @@
+package msa.lesson3;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class Lesson3ApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
