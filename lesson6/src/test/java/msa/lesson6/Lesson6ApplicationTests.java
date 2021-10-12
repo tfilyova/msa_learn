5d5f14bf5ce600b983cc920adef1038dc6092f30
tfilyova <88984709+tfilyova@users.noreply.github.com>
2021-10-12 09:22:40 +0300

    Add files via upload

---
+++ lesson6/src/test/java/msa/lesson6/Lesson6ApplicationTests.java
@@ -0,0 +1,13 @@
+package msa.lesson6;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class Lesson6ApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
