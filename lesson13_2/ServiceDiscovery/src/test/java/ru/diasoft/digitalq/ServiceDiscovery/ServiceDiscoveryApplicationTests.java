f4c6c959c619806270c288b04abf6efefaf0a9c4
tfilyova <88984709+tfilyova@users.noreply.github.com>
2021-10-14 20:18:53 +0300

    Add files via upload

---
+++ lesson13_2/ServiceDiscovery/src/test/java/ru/diasoft/digitalq/ServiceDiscovery/ServiceDiscoveryApplicationTests.java
@@ -0,0 +1,13 @@
+package ru.diasoft.digitalq.ServiceDiscovery;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ServiceDiscoveryApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
