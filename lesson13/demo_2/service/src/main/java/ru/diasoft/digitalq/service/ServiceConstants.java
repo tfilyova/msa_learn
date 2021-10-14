ef43f7ef9100cc761263197e23a25bc731f8128a
tfilyova <88984709+tfilyova@users.noreply.github.com>
2021-10-14 20:03:01 +0300

    Delete lesson13/demo_2 directory

--- lesson13/demo_2/service/src/main/java/ru/diasoft/digitalq/service/ServiceConstants.java
+++
@@ -1,8 +0,0 @@
-package ru.diasoft.digitalq.service;
-
-public class ServiceConstants {
-    private ServiceConstants(){}
-
-    public final static String STATUS_OK = "OK";
-    public final static String STATUS_WAITING = "WAITING";
-}
