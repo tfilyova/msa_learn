c035c5d7e3a20936c76f3a9f781dd6899de94e87
tfilyova <88984709+tfilyova@users.noreply.github.com>
2021-10-14 20:09:47 +0300

    Add files via upload

---
+++ lesson13_2/demo/service/src/main/java/ru/diasoft/digitalq/service/ServiceConstants.java
@@ -0,0 +1,8 @@
+package ru.diasoft.digitalq.service;
+
+public class ServiceConstants {
+    private ServiceConstants(){}
+
+    public final static String STATUS_OK = "OK";
+    public final static String STATUS_WAITING = "WAITING";
+}
